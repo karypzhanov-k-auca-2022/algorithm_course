40b64a5827f6807412fee44cd05fd3bb923e18b2
kai <kairat_k@auca.kg>
2024-01-31 22:08:26 +0600

    Accpeted oj02

--- oj/oj02.cpp
+++ oj/oj02.cpp
@@ -7,17 +7,33 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
+
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
+    TreeNode *sortedArrayToBST(vector<int> &nums) {
         int begin = 0;
-        int end  = nums.size()-1;
+        int end = nums.size() - 1;
 
+        return createTree(begin, end, nums);
+    }
+
+    TreeNode *createTree(int begin, int end, vector<int> &nums) {
+        if (begin > end) return nullptr;
+
+        int mid = begin + (end - begin) / 2;
+        TreeNode* node = new TreeNode(nums[mid]);
+
+        node->left = createTree(begin,mid-1,nums);
+        node->right = createTree(mid+1,end,nums);
+
+        return node;
     }
 
 };
