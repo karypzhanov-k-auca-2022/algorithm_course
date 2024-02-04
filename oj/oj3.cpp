89c82547222b80f2564e7f8bcce47ac19a2be29a
kai <kairat_k@auca.kg>
2024-02-04 20:08:17 +0600

    Wrong answer oj03

---
+++ oj/oj3.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+
+using namespace std;
+
+class TreeNode {
+
+private:
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode()
+            : left(nullptr), right(nullptr) {
+    }
+
+public:
+
+    static bool isPerfect(TreeNode *root) {
+        if (!root) return true; //  is perfect if empty
+
+        int leftDepth = depth(root->left); //  left depth
+        int rightDepth = depth(root->right); //  right depth
+
+        return (leftDepth == rightDepth);
+    }
+
+    static int depth(TreeNode *node) {
+        if (!node) {
+            return 0; // empty tree is 0
+        }
+
+        int leftDepth = depth(node->left); // left depth
+        int rightDepth = depth(node->right); // right depth
+
+        return 1 + max(leftDepth, rightDepth); // add +1 each time, if null then +0
+    }
+
+    static TreeNode *leaf() {
+        return new TreeNode();
+    }
+
+    static TreeNode *join(TreeNode *left, TreeNode *right) {
+        return (new TreeNode())->withChildren(left, right);
+    }
+
+    TreeNode *withLeft(TreeNode *left) {
+        this->left = left;
+        return this;
+    }
+
+    TreeNode *withRight(TreeNode *right) {
+        this->right = right;
+        return this;
+    }
+
+    TreeNode *withChildren(TreeNode *left, TreeNode *right) {
+        this->left = left;
+        this->right = right;
+        return this;
+    }
+
+    TreeNode *withLeftLeaf() {
+        return withLeft(leaf());
+    }
+
+    TreeNode *withRightLeaf() {
+        return withRight(leaf());
+    }
+
+    TreeNode *withLeaves() {
+        return withChildren(leaf(), leaf());
+    }
+
+};
