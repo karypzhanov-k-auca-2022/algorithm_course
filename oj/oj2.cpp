c974b2e453743893d42c552a76891c4b6a60641f
kai <kairat_k@auca.kg>
2024-02-04 20:07:06 +0600

    Accepted oj02

--- oj/oj2.cpp
+++ oj/oj2.cpp
@@ -1,7 +1,7 @@
-#include "iostream"
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
 
 struct TreeNode {
     int val;
@@ -18,24 +18,22 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode *sortedArrayToBST(vector<int> &nums) {
-        int begin = 0;
-        int end = nums.size() - 1;
+        int begin = 0;    // start
+        int end = nums.size() - 1; // finish
 
         return createTree(begin, end, nums);
     }
 
     TreeNode *createTree(int begin, int end, vector<int> &nums) {
-        if (begin > end) return nullptr;
+        if (begin > end) return nullptr;  // base case
 
         int mid = begin + (end - begin) / 2;
-        TreeNode* node = new TreeNode(nums[mid]);
 
-        node->left = createTree(begin,mid-1,nums);
-        node->right = createTree(mid+1,end,nums);
+        TreeNode *node = new TreeNode(nums[mid]); // create new node with mid value
+
+        node->left = createTree(begin, mid - 1, nums);
+        node->right = createTree(mid + 1, end, nums);
 
         return node;
     }
-
 };
-
-
