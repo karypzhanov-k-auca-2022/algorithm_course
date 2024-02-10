6cc8ecdc24675c4dd0513e72f3f37818b546fbcd
kai <kairat_k@auca.kg>
2024-02-10 19:25:54 +0600

    Wrong answer oj10

---
+++ oj/oj10.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string n;
+    int k;
+    cin >> n >> k;
+
+    string N = "";
+
+    for (int i = 0; i < k; ++i) {
+        N += n;
+    }
+
+    int res = 0;
+    int resF = 0;
+
+    for (char i: N) {
+        res += i - '0';
+    }
+
+    N = to_string(res);
+
+    for (char c: N) {
+        resF += c - '0';
+    }
+
+    cout << resF << "\n";
+
+    return 0;
+}
