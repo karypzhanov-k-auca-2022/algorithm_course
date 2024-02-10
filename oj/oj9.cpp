f7943602a0d55fe77a9e84ab4f0386346ef2e5cf
kai <kairat_k@auca.kg>
2024-02-10 19:07:33 +0600

    Accepted oj9

--- oj/oj9.cpp
+++ oj/oj9.cpp
@@ -4,89 +4,82 @@
 
 using namespace std;
 
-char star = '*';
-
 bool hasQueenInRow(const vector<string> &board, int row, int col);
-
 bool hasQueenInColumn(const vector<string> &board, int row, int col);
-
 bool hasQueenInDiagonal(const vector<string> &board, int row, int col);
-
 bool isValidPosition(const vector<string> &board, int row, int col);
+
+char queen = '*';
 
 bool isValid(const vector<string> &board) {
     int queens = 0;
 
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            if (board[i][j] == star) {
+            if (board[i][j] == queen) { // find
                 queens++;
                 if (!isValidPosition(board, i, j)) {
-                    return false;
+                    return false; // immediately false
                 }
             }
         }
     }
 
-    return queens == 8;
+    if (queens == 8) return true;
+    else return false;
 }
 
 bool isValidPosition(const vector<string> &board, int row, int col) {
+    // if queen pass all cases then true
     return !(hasQueenInRow(board, row, col) || hasQueenInColumn(board, row, col) ||
              hasQueenInDiagonal(board, row, col));
 }
 
+// check same row
 bool hasQueenInRow(const vector<string> &board, int row, int col) {
     for (int i = 0; i < 8; i++) {
-        if (i != col && board[row][i] == star) {
+        if (i != col && board[row][i] == queen) {
+            // check other queen from column
             return true;
         }
     }
     return false;
 }
 
+// check same column
 bool hasQueenInColumn(const vector<string> &board, int row, int col) {
     for (int i = 0; i < 8; i++) {
-        if (i != row && board[i][col] == star) {
+        if (i != row && board[i][col] == queen) {
+            // check other queen from row
             return true;
         }
     }
     return false;
 }
 
+// check same diagonal
 bool hasQueenInDiagonal(const vector<string> &board, int row, int col) {
     for (int i = 0; i < 8; i++) {
-        if (row + i < 8 && col + i < 8 && board[row + i][col + i] == star) {
-            return true;
-        }
-        if (row + i < 8 && col - i >= 0 && board[row + i][col - i] == star) {
-            return true;
-        }
-        if (row - i >= 0 && col + i < 8 && board[row - i][col + i] == star) {
-            return true;
-        }
-        if (row - i >= 0 && col - i >= 0 && board[row - i][col - i] == star) {
-            return true;
+        for (int j = 0; j < 8; j++) {
+            if (i != row && j != col && abs(row - i) == abs(col - j) && board[i][j] == queen) {
+
+                return true;
+            }
         }
     }
     return false;
 }
-
 
 int main() {
     vector<string> board;
 
-    int i = 0;
-    while (i < 8) {
+    for (int i = 0; i < 8; i++) {
         string row;
         cin >> row;
-        board.push_back(row);
-        i++;
+        board.push_back(row); // each line
     }
 
-    bool isCorrect = isValid(board);
-
-    if (isCorrect) {
+    if (isValid(board)) {
         cout << "valid" << endl;
     } else {
         cout << "invalid" << endl;
