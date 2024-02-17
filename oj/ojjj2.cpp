12b0cf9414484a5b9aec5069a7b3a07984c2994e
kai <kairat_k@auca.kg>
2024-02-17 19:53:23 +0600

    Accepted ojjj2

--- oj/ojjj2.cpp
+++ oj/ojjj2.cpp
@@ -1,72 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-const int N = 8;
+const int board = 9;
+vector<int> targetPositions(board);
+vector<int> currentPositions(board);
+int moveCount;
 
-bool isSafe(vector<vector<int>>& board, int row, int col) {
-    for (int i = 0; i < col; i++) {
-        if (board[row][i] == 1) {
-            return false;
+bool isAvailable(int row, int col) {
+    for (int i = 1; i < col; i++) {
+        if (row == currentPositions[i] || abs(row - currentPositions[i]) == abs(col - i)) {
+            return false; // If there's a conflict
         }
     }
-
-    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-        if (board[i][j] == 1) {
-            return false;
-        }
-    }
-
-    for (int i = row, j = col; i < N && j >= 0; i++, j--) {
-        if (board[i][j] == 1) {
-            return false;
-        }
-    }
-
-    return true;
+    return true; // no conflict
 }
 
-int solveNQueens(vector<vector<int>>& board, int col) {
-    if (col >= N) {
-        return 0;
-    }
-
-    int min_moves = N;
-
-    for (int i = 0; i < N; i++) {
-        if (isSafe(board, i, col)) {
-            board[i][col] = 1;
-
-            int moves = solveNQueens(board, col + 1);
-
-            min_moves = min(min_moves, moves + (i != col)); // If i equals col, no move is needed
-
-            board[i][col] = 0;
+void minSteps(int col) {
+    for (int row = 1; row < board; row++) {
+        if (isAvailable(row, col)) {
+            currentPositions[col] = row; // place the queen at (row, col)
+            if (col == 8) {
+                int moves = 0;
+                for (int i = 1; i < board; i++) {
+                    if (targetPositions[i] != currentPositions[i]) {
+                        moves++;
+                    }
+                }
+                moveCount = min(moveCount, moves); // Update the minimum move count
+            } else {
+                minSteps(col + 1); // for the next column
+            }
         }
     }
-
-    return min_moves;
 }
+
 
 int main() {
-    int t = 1;
-    vector<vector<int>> board(N, vector<int>(N, 0));
-    vector<int> queens(N);
-    while (cin >> queens[0]) {
-        for (int i = 1; i < N; i++) {
-            cin >> queens[i];
+    int caseNum = 1;
+    do {
+        for (int i = 1; i < board; i++) {
+            int num;
+            if (!(cin >> num)) {
+                return 0; // exit
+            }
+            targetPositions[i] = num;
         }
 
-        for (int i = 0; i < N; i++) {
-            board[queens[i] - 1][i] = 1;
-        }
-
-        int min_moves = solveNQueens(board, 0);
-
-        cout << "Case " << t++ << ": " << min_moves << endl;
-
-        board.assign(N, vector<int>(N, 0));
-    }
+        moveCount = 1000;
+        minSteps(1); // Start from first column
+        cout << "Case " << caseNum++ << ": " << moveCount << endl;
+    } while (true);
 }
