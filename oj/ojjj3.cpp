73ea5ac2e0f160886b7719a487fe1a761e8cf940
kai <kairat_k@auca.kg>
2024-02-17 17:23:54 +0600

    Wrong answer ojjj3

---
+++ oj/ojjj3.cpp
@@ -0,0 +1,18 @@
+#include <climits>
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool checkBST_helper(Node* root, int min, int max) {
+    if (root == nullptr) return true;
+    if (root->data < min || root->data > max) return false;
+    return checkBST_helper(root->left, min, root->data - 1) && checkBST_helper(root->right, root->data + 1, max);
+}
+
+bool checkBST(Node* root) {
+    return checkBST_helper(root, 0, 1000);
+}
+
