28cb1d5c08add8df4597186e45c77cb1fc851a60
kai <kairat_k@auca.kg>
2024-03-05 23:46:32 +0600

    fun1: try to fix bugs because my code didn't work on the lab computers that are on windows

--- stuff/test_spaces.cpp
+++ stuff/test_spaces.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main() {
-    cout << "What is your name?:         ";
+    cout << "What is your name?:     ";
     string name;
     cin >> name;
-    cout << "Hello, " << name << "!\n";
+    cin.clear();    //  clear the input buffer
+
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');    //  ignore the rest of the line (if any
+
+
+    cout << "Hello, " << name << "!" << endl;
 
     return 0;
 }
