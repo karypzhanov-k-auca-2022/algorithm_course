79b7d7bfb924e02054be1105ce2c3a5379349bc2
Kairat Karypzhanov <kairat_k@auca.kg>
2024-03-05 23:18:19 +0600

    check from Windows for the bugs

---
+++ stuff/test_spaces.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    cout << "What is your name?:     ";
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
