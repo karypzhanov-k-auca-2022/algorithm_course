2936076cca51f7a3ed4acde54525003a59a806dc
kai <kairat_k@auca.kg>
2024-04-01 23:03:17 +0600

    fun3: read parts from file

--- fun3/main.cpp
+++ fun3/main.cpp
@@ -1,44 +1,79 @@
-#include "iostream"
-#include "vector"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
+void print_line() {
+    cout << "-----------------------------------" << endl;
+}
+
+struct Budget {
+    int financial;
+    int resource;
+    int ecological;
+};
+
 int main() {
     vector<string> ddata = {"data1", "data2"};
-    cout << "data1 or data2? ";
+    cout << "data1 - 1" << endl;
+    cout << "data2 - 2" << endl;
     int data;
     cin >> data;
 
+    if (data < 1 || data > ddata.size()) {
+        cout << "Invalid data number!" << endl;
+        return 1;
+    }
+
     string file_name_to_read = "../fun3/" + ddata[data - 1] + ".txt";
 
-    ifstream file1ToRead(file_name_to_read);
+    ifstream file(file_name_to_read);
     string line;
-    vector<int> budget;
-    vector<int> impact;
-    vector<int> resources;
-    vector<int> capacity;
 
-    int max_budget = 1000;
-    int max_impact = 100;
-    int max_resources = 150;
+    // data
+    Budget budget;
+    budget = {0, 0, 0};
+    vector<int> cost;
+    vector<int> capacity_increase;
+    vector<int> resources_required;
+    vector<int> impact_score;
 
 
-    vector<vector<vector<vector<int>>>> dp(max_budget + 1, vector<vector<vector<int>>>(max_impact + 1,
-                                                                                       vector<vector<int>>(
-                                                                                               max_resources + 1,
-                                                                                               vector<int>(5 + 1,
-                                                                                                           -1))));
-    for (int i = 0; i <= max_budget; i++) {
-        for (int j = 0; j <= max_impact; j++) {
-            for (int k = 0; k <= max_resources; k++) {
-                for (int l = 0; l <= 5; l++) {
-                    if (i == 0 || j == 0 || k == 0 || l == 0) {
-                        dp[i][j][k][l] = 0;
-                    }
-                }
-            }
+    while (getline(file, line)) {
+        stringstream eachLine(line);
+        string word1, word2, word3;
+        getline(eachLine, word1, ' ');
+        getline(eachLine, word2, ' ');
+        getline(eachLine, word3, ' ');
+
+        if (word1 == "financial") {
+            budget.financial = stoi(word3);
+        } else if (word1 == "resource") {
+            budget.resource = stoi(word3);
+        } else if (word1 == "ecological") {
+            budget.ecological = stoi(word3);
         }
+
+
+//        string part1, part2, part3, part4;
+//        getline(eachLine, part1, ',');
+//        getline(eachLine, part2, ',');
+//        getline(eachLine, part3, ',');
+//        getline(eachLine, part4, ' ');
+//
+//        stringstream ss1(part1);
+//        stringstream ss2(part2);
+//        stringstream ss3(part3);
+//        stringstream ss4(part4);
+
+
     }
+
+    cout << "Financial budget: " << budget.financial << endl;
+    cout << "Resource budget: " << budget.resource << endl;
+    cout << "Ecological budget: " << budget.ecological << endl;
 
     return 0;
 }
