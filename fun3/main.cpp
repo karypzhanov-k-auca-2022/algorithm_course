6c1898a8b7fe4915c404cff0b6698af774a785a1
kai <kairat_k@auca.kg>
2024-04-02 01:13:27 +0600

    fun3: read file correctly and fill with -1

--- fun3/main.cpp
+++ fun3/main.cpp
@@ -19,43 +19,56 @@ struct Option {
 };
 
 int main() {
-    vector<string> ddata = {"data1", "data2"};
+    vector<string> ddata = {"data1", "data2"}; // data files
     cout << "data1 - 1" << endl;
     cout << "data2 - 2" << endl;
     int data;
     cin >> data;
 
     if (data < 1 || data > ddata.size()) {
-        cout << "Invalid data number!" << endl;
+        cout << "Invalid data number!" << endl; // check if the input is valid
         return 1;
     }
 
-    string file_name_to_read = "../fun3/" + ddata[data - 1] + ".txt";
+    string file_name_to_read = "../fun3/" + ddata[data - 1] + ".txt"; // file to read
 
     ifstream file(file_name_to_read);
     string line;
 
-    // data
     Budget budget;
     budget = {0, 0, 0};
     vector<Option> options;
 
+    // read 3 lines of the file
     int n = 0;
+    while (getline(file, line)) {
+        stringstream eachLine(line); // read each line
+        string word1, word2, word3;
+        getline(eachLine, word1, ' ');
+        getline(eachLine, word2, ' ');
+        getline(eachLine, word3, ' ');
+
+        if (word1 == "financial") {
+            budget.financial = stoi(word3);
+        } else if (word1 == "resource") {
+            budget.resource = stoi(word3);
+        } else if (word1 == "ecological") {
+            budget.ecological = stoi(word3);
+        }
+
+        n++;
+        if (n == 3) {
+            break;
+        }
+    }
+
+    // read the rest of the file
     while (getline(file, line)) {
         stringstream eachLine(line);
         string word;
-        getline(eachLine, word, ':');
+        getline(eachLine, word, ':'); // read until ':'
 
-        if (word == "financial") {
-            getline(eachLine, word, ' ');
-            budget.financial = stoi(word);
-        } else if (word == "resource") {
-            getline(eachLine, word, ' ');
-            budget.resource = stoi(word);
-        } else if (word == "ecological") {
-            getline(eachLine, word, ' ');
-            budget.ecological = stoi(word);
-        } else if (word == "cost") {
+        if (word == "cost") {
             Option option;
             getline(eachLine, word, ',');
             option.cost = stoi(word);
@@ -81,6 +94,13 @@ int main() {
              << ", resources required " << option.resources_required << ", impact score " << option.impact_score
              << endl;
     }
+
+    vector<vector<vector<vector<int>>>> dp(3, vector<vector<vector<int>>>(budget.financial + 1,
+                                                                          vector<vector<int>>(budget.resource + 1,
+                                                                                              vector<int>(
+                                                                                                      budget.ecological +
+                                                                                                      1, -1))));  // fill with -1
+
 
     return 0;
 }
