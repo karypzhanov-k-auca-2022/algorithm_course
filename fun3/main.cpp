fbb0bf3ff39a584fa26ea6f72a5aef1c7f629238
kai <kairat_k@auca.kg>
2024-04-14 15:41:21 +0600

    fun3: add menu to choose which file to read and selection to out all answers from each data file

--- fun3/main.cpp
+++ fun3/main.cpp
@@ -43,6 +43,37 @@ int dp_recursive(vector<Option> &options, Budget &budget, int i, int j, int k, i
                + options[i - 1].capacity_increase); // update
 }
 
+// Iterative
+int dp_iterative(vector<Option> &options, Budget &budget, int i, int j, int k, int l) {
+    vector<vector<vector<vector<int>>>> dp(options.size() + 1, vector<vector<vector<int>>>(budget.financial + 1,
+                                                                                           vector<vector<int>>(
+                                                                                                   budget.resource + 1,
+                                                                                                   vector<int>(
+                                                                                                           budget.ecological +
+                                                                                                           1,
+                                                                                                           0)))); // fill with 0
+
+    for (int i = 1; i <= options.size(); i++) {
+        for (int j = 1; j <= budget.financial; j++) {
+            for (int k = 1; k <= budget.resource; k++) {
+                for (int l = 1; l <= budget.ecological; l++) {
+                    dp[i][j][k][l] = dp[i - 1][j][k][l];
+                    if (options[i - 1].cost <= j && options[i - 1].resources_required <= k &&
+                        options[i - 1].impact_score <= l) { // check if the option can be selected
+                        dp[i][j][k][l] = max(dp[i - 1][j][k][l], // do not update
+                                             dp[i - 1][j - options[i - 1].cost]
+                                             [k - options[i - 1].resources_required]
+                                             [l - options[i - 1].impact_score]
+                                             + options[i - 1].capacity_increase); // update
+                    }
+                }
+            }
+        }
+    }
+
+    return dp.back().back().back().back();
+}
+
 void data_menu() {
     cout << "Select data (1 - 3):" << endl;
     cout << "data1 - 1" << endl;
@@ -127,75 +158,134 @@ bool read_from_json(string &file_name, Budget &budget, vector<Option> &options) 
     return true;
 }
 
+void print_line() {
+    cout << "-----------------------------------------------------------------" << endl;
+}
+
 int main() {
+    int choice;
+    cout << "Menu (1) or see all answers from each data file (2)" << endl;
+    cout << "Select 1 or 2:" << endl;
+    cin >> choice;
+
+    // error handling
+    if (choice < 1 || choice > 2) {
+        cerr << "Invalid choice selected" << endl;
+        cerr << "Please select choice from 1 or 2" << endl;
+        return 1;
+    }
+
     vector<string> ddata = {"data1", "data2", "data3"}; // data files
 
-    cout << "Select data format (1 - JSON, 2 - TXT): ";
-    int number;
-    cin >> number;
+    if (choice == 1) {
+        Budget budget = {0, 0, 0};
+        vector<Option> options;
+        cout << "Select data format (1 - JSON, 2 - TXT) or to see all answers (3):" << endl;
+        int number;
+        cin >> number;
 
-    // error handling
-    if (number < 1 || number > 2){
-        cerr << "Invalid data format selected" << endl;
-        cerr << "Please select data format from 1 or 2" << endl;
-        return 1;
-    }
+        // error handling
+        if (number < 1 || number > 2) {
+            cerr << "Invalid data format selected" << endl;
+            cerr << "Please select data format from 1 or 2" << endl;
+            return 1;
+        }
 
-    string file_format = (number == 1) ? "json" : "txt"; // file format
-    Budget budget = {0, 0, 0};
-    vector<Option> options;
+        string file_format = (number == 1) ? "json" : "txt"; // file format
 
-    data_menu();
-    int data;
-    cin >> data;
 
-    // error handling
-    if (data < 1 || data > 3){
-        cerr << "Invalid data selected" << endl;
-        cerr << "Please select data from 1 to 3" << endl;
-        return 1;
-    }
+        data_menu();
+        int data;
+        cin >> data;
 
-    string file_name_to_read = "../fun3/" + ddata[data - 1] + "." + file_format; // file to read
+        // error handling
+        if (data < 1 || data > 3) {
+            cerr << "Invalid data selected" << endl;
+            cerr << "Please select data from 1 to 3" << endl;
+            return 1;
+        }
 
-    if (file_format == "json") {
-        read_from_json(file_name_to_read, budget, options);
-    }
+        string file_name_to_read = "../fun3/" + ddata[data - 1] + "." + file_format; // file to read
 
-    if (file_format == "txt") {
-        read_from_txt(file_name_to_read, budget, options);
-    }
+        if (file_format == "json") {
+            read_from_json(file_name_to_read, budget, options);
+        }
 
-    vector<vector<vector<vector<int>>>> dp(options.size() + 1, vector<vector<vector<int>>>(budget.financial + 1,
-                                                                                           vector<vector<int>>(
-                                                                                                   budget.resource + 1,
-                                                                                                   vector<int>(
-                                                                                                           budget.ecological +
+        if (file_format == "txt") {
+            read_from_txt(file_name_to_read, budget, options);
+        }
+
+        vector<vector<vector<vector<int>>>> dp(options.size() + 1, vector<vector<vector<int>>>(budget.financial + 1,
+                                                                                               vector<vector<int>>(
+                                                                                                       budget.resource +
+                                                                                                       1,
+                                                                                                       vector<int>(
+                                                                                                               budget.ecological +
+                                                                                                               1,
+                                                                                                               0)))); // fill with 0
+
+        int iterative_result = dp_iterative(options, budget, options.size(), budget.financial, budget.resource,
+                                            budget.ecological);
+        int recursive_result = dp_recursive(options, budget, options.size(), budget.financial, budget.resource,
+                                            budget.ecological);
+
+        cout << "Maximum flights served (Recursive): " << recursive_result << endl;
+        cout << "Maximum flights served (Non-Recursive): " << iterative_result << endl;
+    } else if (choice == 2) {
+        // JSON output
+        for (int i = 0; i < 3; i++) {
+            Budget budget = {0, 0, 0};
+            vector<Option> options;
+            string file_name_to_read = "../fun3/" + ddata[i] + ".json"; // file to read
+            read_from_json(file_name_to_read, budget, options);
+
+            vector<vector<vector<vector<int>>>> dp(options.size() + 1, vector<vector<vector<int>>>(budget.financial + 1,
+                                                                                                   vector<vector<int>>(
+                                                                                                           budget.resource +
                                                                                                            1,
-                                                                                                           0)))); // fill with 0
+                                                                                                           vector<int>(
+                                                                                                                   budget.ecological +
+                                                                                                                   1,
+                                                                                                                   0)))); // fill with 0
 
-    // Iterative
-    for (int i = 1; i <= options.size(); i++) {
-        for (int j = 1; j <= budget.financial; j++) {
-            for (int k = 1; k <= budget.resource; k++) {
-                for (int l = 1; l <= budget.ecological; l++) {
-                    dp[i][j][k][l] = dp[i - 1][j][k][l];
-                    if (options[i - 1].cost <= j && options[i - 1].resources_required <= k &&
-                        options[i - 1].impact_score <= l) { // check if the option can be selected
-                        dp[i][j][k][l] = max(dp[i - 1][j][k][l], // do not update
-                                             dp[i - 1][j - options[i - 1].cost]
-                                             [k - options[i - 1].resources_required]
-                                             [l - options[i - 1].impact_score]
-                                             + options[i - 1].capacity_increase); // update
-                    }
-                }
-            }
+
+            int iterative_result = dp_iterative(options, budget, options.size(), budget.financial, budget.resource,
+                                                budget.ecological);
+            int recursive_result = dp_recursive(options, budget, options.size(), budget.financial, budget.resource,
+                                                budget.ecological);
+
+            cout << "Data " << "(JSON) " << i + 1 << endl;
+            cout << "Maximum flights served (Recursive): " << recursive_result << endl;
+            cout << "Maximum flights served (Non-Recursive): " << iterative_result << endl;
+
+        }
+        print_line();
+        // TXT output
+        for (int i = 0; i < 3; i++) {
+            Budget budget = {0, 0, 0};
+            vector<Option> options;
+            string file_name_to_read = "../fun3/" + ddata[i] + ".txt"; // file to read
+            read_from_txt(file_name_to_read, budget, options);
+
+            vector<vector<vector<vector<int>>>> dp(options.size() + 1, vector<vector<vector<int>>>(budget.financial + 1,
+                                                                                                   vector<vector<int>>(
+                                                                                                           budget.resource +
+                                                                                                           1,
+                                                                                                           vector<int>(
+                                                                                                                   budget.ecological +
+                                                                                                                   1,
+                                                                                                                   0)))); // fill with 0
+
+            int iterative_result = dp_iterative(options, budget, options.size(), budget.financial, budget.resource,
+                                                budget.ecological);
+            int recursive_result = dp_recursive(options, budget, options.size(), budget.financial, budget.resource,
+                                                budget.ecological);
+
+            cout << "Data " << "(TXT) " << i + 1 << endl;
+            cout << "Maximum flights served (Recursive): " << recursive_result << endl;
+            cout << "Maximum flights served (Non-Recursive): " << iterative_result << endl;
         }
     }
-
-    cout << "Maximum flights served (Recursive): "
-         << dp_recursive(options, budget, options.size(), budget.financial, budget.resource, budget.ecological) << endl;
-    cout << "Maximum flights served (Non-Recursive): " << dp.back().back().back().back() << endl;
 
     return 0;
 }
