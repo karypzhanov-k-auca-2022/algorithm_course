6e5ebfe7501abcd55c3aaa95a50065045fb4a426
kai <kairat_k@auca.kg>
2024-05-08 17:42:47 +0600

    oj19: Accepted

--- oj/oj19.cpp
+++ oj/oj19.cpp
@@ -1,8 +1,21 @@
 #include <vector>
+#include <iostream>
+#include <cstdint>
 
 std::vector<uint64_t> powers_of_two(int n) {
     std::vector<uint64_t> result;
-    for (int h: result) {
-        result.push_back(1 << h);
+    for (int i = 0; i <= n; i++) {
+        result.push_back(1ULL << i);
     }
+    return result;
+}
+
+int main() {
+    int n = 54;
+    std::vector<uint64_t> result = powers_of_two(n);
+
+    for (uint64_t num : result) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
 }
